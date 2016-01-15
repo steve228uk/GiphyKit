8fe799b2f25c78c95e6288b57773717c3dec59d1
Stephen Radford <steve228uk@gmail.com>
2016-01-15 14:02:45 +0000

    Complete search to map to a single GIF. Will pull the rest out when I can be fucked

--- GIF.swift
+++ GIF.swift
@@ -35,10 +35,50 @@ extension Giphy {
         Alamofire.request(API.Search(query))
             .responseJSON { response in
                 if response.result.isSuccess {
-                    Swift.print(response.result.value)
+                    if let value = response.result.value {
+                        let json = JSON(value)
+                        if let data = json["data"].array {
+                            let gifs = data.map(parseGIF)
+                            callback(gifs, response.result.error)
+                            return
+                        }
+                    }
                 }
+                
+                callback([], response.result.error)
             }
         
+    }
+    
+    /**
+     Parse JSON to Giphy GIF
+     
+     - parameter json: Raw JSON
+     
+     - returns: A shiny shiny GIF
+     */
+    internal class func parseGIF(json: JSON) -> GIF {
+        var gif = GIF()
+        gif.id = json["id"].string
+        gif.bitlyURL = json["bitly_url"].string
+        gif.bitlyGIFURL = json["bitly_gif_url"].string
+        if let images = json["images"].dictionary {
+            if let image = images["original"]?.dictionary {
+                
+                var img = GiphyImage()
+                img.height = image["height"]?.string
+                img.width = image["width"]?.string
+                img.mp4 = image["mp4"]?.string
+                img.mp4Size = image["mp4_size"]?.string
+                img.size = image["size"]?.string
+                img.url = image["url"]?.string
+                img.webp = image["webp"]?.string
+                img.webpSize = image["webp_size"]?.string
+                gif.images = [img]
+                
+            }
+        }
+        return gif
     }
     
 }
