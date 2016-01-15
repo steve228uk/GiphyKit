5284a32d54e835c3f66d10f30604be93b96a33a3
Stephen Radford <steve228uk@gmail.com>
2016-01-15 12:20:42 +0000

    Initial commit

---
+++ Giphy.swift
@@ -0,0 +1,10 @@
+//
+//  Giphy.swift
+//  Peach
+//
+//  Created by Stephen Radford on 15/01/2016.
+//  Copyright © 2016 Cocoon Development Ltd. All rights reserved.
+//
+
+import Foundation
+
---
+++ GiphyKit.h
@@ -0,0 +1,19 @@
+//
+//  GiphyKit.h
+//  GiphyKit
+//
+//  Created by Stephen Radford on 15/01/2016.
+//  Copyright © 2016 Cocoon Development Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GiphyKit.
+FOUNDATION_EXPORT double GiphyKitVersionNumber;
+
+//! Project version string for GiphyKit.
+FOUNDATION_EXPORT const unsigned char GiphyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiphyKit/PublicHeader.h>
+
+
